c231d396ed9fcfef5434c7f5176b8a5d9d0a289c
Plutonicx <stojanovich.stevan@live.com>
2015-07-26 14:27:19 +1000

    Latest changes.  - gradient descent is correct though its slow. Could try recursively changing alpha if the differential changes in abs(max(delta)) is too small. - Using glm(binomial dist.) to do the log. reg. Results seem pretty accurate - Clean up training data - Added in test data (may need to download this additionally from MNIST).

--- logreg.R
+++ logreg.R
@@ -9,80 +9,82 @@ rm(list = ls())
 
 # check out https://gist.github.com/johnbaums/882ad1e458e13b96a3d1 For the original code.
 # this reads the MNIST data.
-  
-  readMNIST <- function(image = "R/ex1/train-images.idx3-ubyte", pred = "R/ex1/train-labels.idx1-ubyte") {
-    img <- file(image, "rb")
-    hdr <- readBin(img, 'integer', n=4, endian="big")
-    X <- t(sapply(seq_len(hdr[2]), function(ii) {
-      readBin(img, 'integer', size=1, n=prod(hdr[3:4]), endian="big")
-    }))  
-    close(img)
-    lab <- file(pred, "rb")
-    readBin(lab, "integer", n = 1, size = 4, endian = "big")
-    n <- readBin(lab, integer(), n=1, size=4, endian="big")
-    y <- readBin(lab, integer(), n=n, size=1, signed=FALSE)  
-    close(lab)
-    cbind.data.frame(y=factor(y), X)
-  }  
-  X <- readMNIST()
 
-  # only get 0's and 1's for this example.
-  x <- X[X$y == 1 | X$y == 0,]
-  y <- x[,1]
-  
-  #shift the data by the mean and scale according to the standard deviation. 
-  m <- rowMeans(x[,2:ncol(x)], dims = 1)
-  s <- apply(x[,2:ncol(x)],1,sd)
+readMNIST <- function(image = "R/ex1/train-images.idx3-ubyte", pred = "R/ex1/train-labels.idx1-ubyte") {
+  img <- file(image, "rb")
+  hdr <- readBin(img, 'integer', n=4, endian="big")
+  X <- t(sapply(seq_len(hdr[2]), function(ii) {
+    readBin(img, 'integer', size=1, n=prod(hdr[3:4]), endian="big")
+  }))  
+  close(img)
+  lab <- file(pred, "rb")
+  readBin(lab, "integer", n = 1, size = 4, endian = "big")
+  n <- readBin(lab, integer(), n=1, size=4, endian="big")
+  y <- readBin(lab, integer(), n=n, size=1, signed=FALSE)  
+  close(lab)
+  cbind.data.frame(y=factor(y), X)
+}  
+X <- readMNIST()
 
-  #Get our normalised training set.
-  Train <- data.frame(y,(x[,2:ncol(x)] - m) / (s+0.1))
+# only get 0's and 1's for this example.
+x <- X[X$y == 1 | X$y == 0,]
+y <- x[,1]
 
-  #set up an index and permute the training set.
-  index <- 1:nrow(Train)
-  permIndex <- sample(index)
-  
-  Train <- Train[permIndex,]
+#shift the data by the mean and scale according to the standard deviation. 
+m <- rowMeans(x[,2:ncol(x)], dims = 1)
+s <- apply(x[,2:ncol(x)],1,sd)
 
+#Get our normalised training set.
+Train <- data.frame(y,(x[,2:ncol(x)] - m) / (s+0.1))
 
-  Do the same for our test set. Set this up as a new function to retrieve the training / test data.
-  Y <- readMNIST(image = "R/ex1/t10k-images.idx3-ubyte", pred = "R/ex1/t10k-labels.idx1-ubyte")
-  
-  x <- X[Y$y == 1 | Y$y == 0,]
-  y <- x[,1]
-  
-  #shift the data by the mean and scale according to the standard deviation. 
-  m <- rowMeans(x[,2:ncol(x)], dims = 1)
-  s <- apply(x[,2:ncol(x)],1,sd)
-  
-  #Get our normalised training set.
-  Test <- data.frame(y,(x[,2:ncol(x)] - m) / (s+0.1))
+#set up an index and permute the training set.
+index <- 1:nrow(Train)
+permIndex <- sample(index)
 
-  
+Train <- Train[permIndex,]
+
+Train$intercept = 1
 
 
-  #test <- read('t10k')
-  #list(train=train, test=test)
+Do the same for our test set. Set this up as a new function to retrieve the training / test data.
+Y <- readMNIST(image = "R/ex1/t10k-images.idx3-ubyte", pred = "R/ex1/t10k-labels.idx1-ubyte")
+
+x <- Y[Y$y == 1 | Y$y == 0,]
+y <- x[,1]
+
+#shift the data by the mean and scale according to the standard deviation. 
+m <- rowMeans(x[,2:ncol(x)], dims = 1)
+s <- apply(x[,2:ncol(x)],1,sd)
+
+#Get our normalised training set.
+Test <- data.frame(y,(x[,2:ncol(x)] - m) / (s+0.1))
+
+Test$intercept = 1
+
+
+#test <- read('t10k')
+#list(train=train, test=test)
 
 
 
 # To visualise the data.
-              # to.read <- file("R/ex1/train-images.idx3-ubyte", "rb")
-              # to.pred <- file("R/ex1/train-labels.idx1-ubyte", "rb")
-              # 
-              # # read the header. 2051 images, 28 rows, 28 columns per image.
-              # readBin(to.read, integer(), n=4, endian="big")
-              # readBin(to.pred, integer(), n=1, endian="big")
-              # 
-              # # read this data into a matrix.
-              # m <- matrix(readBin(to.read,integer(), size=1, n=28*28, endian="big"),28,28)
-              # 
-              # #flip it to make it readable.
-              # m <- apply(m,2,rev)
-              # m <- apply(m,1,rev)
-              # m <- apply(m,1,rev)
-              # 
-              # #have a look at the image.
-              # image(m)
+#                to.read <- file("R/ex1/train-images.idx3-ubyte", "rb")
+#                to.pred <- file("R/ex1/train-labels.idx1-ubyte", "rb")
+#                
+#                # read the header. 2051 images, 28 rows, 28 columns per image.
+#                readBin(to.read, integer(), n=4, endian="big")
+#                readBin(to.pred, integer(), n=1, endian="big")
+#                
+#                # read this data into a matrix.
+#                m <- matrix(readBin(to.read,integer(), size=1, n=28*28, endian="big"),28,28)
+#                
+#                #flip it to make it readable.
+#                m <- apply(m,2,rev)
+#                m <- apply(m,1,rev)
+#                m <- apply(m,1,rev)
+#                
+#                #have a look at the image.
+#                image(m)
 
 
 
@@ -115,13 +117,13 @@ logregVec = function(x,y,alpha=0.05, maxIterations = 1000000){
   
   # counter to make sure we don't run an infinite loop.
   n = 0
-  #gradient descent
+  #gradient descent. TODO: When delta gets ~ < 10 the algorithm becomes really slow to converge. Might need to dynamically increase alpha depending on the size of norm(delta, "F")
   while (abs(max(delta)) > 0.0001 & n < maxIterations)
   {
     h = sigmoid(x, t(theta))
     err = h - y
     delta = (t(x) %*% t(err))
-    delta = delta / norm(delta, "F")
+    deltaNorm = delta / norm(delta, "F")
     theta = theta - alpha*delta
     n = n+1 
     print(n)
@@ -137,16 +139,27 @@ logregVec = function(x,y,alpha=0.05, maxIterations = 1000000){
 #Run our function. It takes a while. Pun not intended.
 Coefficients <- logregVec(x,y,0.0001)
 
+# the easy way. Even this is really long.
 
-# the easy way...
-fit.lm = lm(V14~.,data=train[,1:14])
+fit.log = glm(y~., data = Train[,2:785], family = binomial )
+
+
 
 # our version of theta
 theta
 
 # R's standard version of theta
-fit.lm
 
+
+T = Test[,2:785]
+p <- predict.glm(fit.log, newdata = T,type = "response")
+
+p = as.matrix(p)
+
+
+pred = as.numeric(p > 0.5)
+
+compare = data.frame(pred,Test)
 
 #Get least squares (training).
 
@@ -166,7 +179,6 @@ testLeastSq <- 0.5*t(testError)%*%testError / nrow(testError)
 
 #test error per observation. This should be higher than the test error per observation (unless we're really lucky!)
 testLeastSq
-
 
 
 
