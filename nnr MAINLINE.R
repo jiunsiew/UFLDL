a75539686da21901bcb0f8e6fe9abac6d74978e8
unknown <stojanovich.stevan@live.com>
2015-09-06 19:20:34 +1000

    cleaned up forward prop.

--- nnr MAINLINE.R
+++ nnr MAINLINE.R
@@ -15,7 +15,7 @@
 housingData <- read.table("R/ex1/housing.data")
 
 # add 'intercept' column
-housingData$intercept = 1
+#housingData$intercept = 1
 
 # split into training and test data
 permVect <- runif(nrow(housingData))
@@ -33,48 +33,33 @@ y <- train[,14]
 # observables (most explanatory from lin reg. This is just a small problem to test NNR.)
 x <- as.matrix(train[,c(5,6,8)])
 
+
+
 nneurons <- ncol(x)
 
 W1 <- as.matrix(rnorm(nneurons^2, mean = 0, sd = 1))
 b1 <- as.matrix(rnorm(nneurons, mean = 0, sd = 1))
+b1 = matrix(b1, nrow = 3, ncol = nrow(x))
 
 W_1 <- matrix(W1, nrow = 3, ncol = 3)
 
-a_1 <- W_1 %*% t(x) + matrix(b1, nrow = 3, ncol = 407)#+ t(b1)
+#a_1 <- t(W_1 %*% t(x) + b1)
 
 
-
-layer1 = function(x){
-  w_1 = W1[1:3,]
-  w_2 = W1[4:6,]
-  w_3 = W1[7:9,]
-
-sigmoid = function(x,theta,b){
-  return (1 / (1 + exp(-1*(theta%*%t(x) + b))))
-}
 
 sg = function(x){
   return (1 / (1 + exp(-1*(x))))
 }
 
-temp <- w_1 %*% t(x) + b1[1]  
+a_2 = sg(t(W_1 %*% t(x) + b1))
+
+
+W_2 <- t(as.matrix(rnorm(nneurons, mean = 0, sd = 1)))
+b2 <- as.matrix(rnorm(1, mean = 0, sd = 1))
+b2 = matrix(b1, nrow = 1, ncol = nrow(x))
+
+a_3 <- sg(t(W_2 %*% t(a_2) + b2)  )
   
-  a_1 = sigmoid(x, w_1, b1[1])
-  a_2 = sigmoid(x, w_2, b1[2])
-  a_3 = sigmoid(x, w_3, b1[3])
-  
-  a = as.matrix(data.frame(t(a_1),t(a_2),t(a_3)))
-  return(a)
-}
-
-b_2 = rnorm(1, mean = 0, sd = 1)
-a = layer1(x)
-
-W2 <- as.matrix(rnorm(nneurons, mean = 0, sd = 1))
-
-output <- sigmoid(a,t(W2),b_2)
-
-output = t(output)
 
 derivSg = function(x){
   sg(x)*(1-sg(x))
