d2a472373607fe907e1453f9a0bf149b61d45314
unknown <stojanovich.stevan@live.com>
2015-09-09 21:52:40 +1000

    created gradient descent algorithm for 1 hidden layer with 3 neurons.
    next step will be to create data source (set rule manually) and try to solve.

--- nnr MAINLINE.R
+++ nnr MAINLINE.R
@@ -7,7 +7,7 @@
 # # observables
 # x <- as.matrix(Train[,2:ncol(Train)])
 
-
+rm(list = ls())
 ## create NNR with 1 hidden layer
 
 ## start off with a simple dataset.. i.e. the housing dataset.
@@ -29,59 +29,116 @@ test <- housingData[!trainIndex,]
 
   
 # predictor
-y <- train[,14]
+Y <- train[,14]
 # observables (most explanatory from lin reg. This is just a small problem to test NNR.)
-x <- as.matrix(train[,c(5,6,8)])
+X <- as.matrix(train[,c(5,6,8)])
 
 
 
 nneurons <- ncol(x)
 
-W1 <- as.matrix(rnorm(nneurons^2, mean = 0, sd = 1))
-b1 <- as.matrix(rnorm(nneurons, mean = 0, sd = 1))
-b1 = matrix(b1, nrow = 3, ncol = nrow(x))
-
-W_1 <- matrix(W1, nrow = 3, ncol = 3)
-
-#a_1 <- t(W_1 %*% t(x) + b1)
-
-a_1 = x
 
 sg = function(x){
   return (1 / (1 + exp(-1*(x))))
 }
-
-z_2 = t(W_1 %*% t(x) + b1)
-a_2 = sg(z_2)
-
-
-W_2 <- t(as.matrix(rnorm(nneurons, mean = 0, sd = 1)))
-b2 <- as.matrix(rnorm(1, mean = 0, sd = 1))
-b2 = matrix(b1, nrow = 1, ncol = nrow(x))
-
-z_3 = t(W_2 %*% t(a_2) + b2)
-a_3 <- sg(z_3)
-  
 
 derivSg = function(x){
   sg(x)*(1-sg(x))
 }
 
 
-dv <- derivSg(z_3)
+set.seed(823)
 
-delta_3 <-  -1*(y - a_3)*derivSg(z_3)
+W1 <- as.matrix(rnorm(nneurons^2, mean = 0, sd = 1))
+b1 <- as.matrix(rnorm(nneurons, mean = 0, sd = 1))
 
-delta_2 <- t(W_2)%*%t(delta_3) * derivSg(t(z_2))
 
-d_2_placeholder <- as.matrix(c(1))
-d_2_placeholder = matrix(tmp, nrow = 1, ncol = nrow(x))
+W_1 <- matrix(W1, nrow = 3, ncol = 3)
 
-nabla_w_1 = delta_2 %*% a_1
-nabla_b_1 = delta_2 %*% t(d_2_placeholder)
+W_2 <- t(as.matrix(rnorm(nneurons, mean = 0, sd = 1)))
+b2 <- as.matrix(rnorm(1, mean = 0, sd = 1))
 
-nabla_w_2 = t(delta_3) %*% a_2
-nabla_b_2 = t(delta_3) %*% t(d_2_placeholder)
+delta_W_1 = matrix(0, dim(W_1)[1], dim(W_1)[2] )
+delta_b_1 = matrix(0, dim(b1))
+
+delta_W_2 = matrix(0, dim(W_2)[1], dim(W_2)[2] )
+delta_b_2 = matrix(0, dim(b2))
+
+
+m = nrow(X)
+lambda = 1
+alpha = 0.005
+
+# counter to make sure we don't run an infinite loop.
+n = 0
+maxComponent = 1
+maxIterations = 100
+#gradient descent. TODO: When delta gets ~ < 10 the algorithm becomes really slow to converge. Might need to dynamically increase alpha depending on the size of norm(delta, "F")
+
+
+gradDescent()
+
+gradDescent = function(){
+while (maxComponent > 0.0001 & n < maxIterations)
+{
+  for (i in 1:nrow(X)){
+    
+    x = X[i,]
+    y = Y[i] / max(Y)
+    #a_1 <- t(W_1 %*% t(x) + b1)
+    
+    a_1 = x
+    
+    
+    
+    z_2 = t(W_1 %*% x + b1)
+    a_2 = sg(z_2)
+    
+    
+    z_3 = t(W_2 %*% t(a_2) + b2)
+    a_3 <- sg(z_3)
+    
+    
+    
+    
+    
+    dv <- derivSg(z_3)
+    
+    delta_3 <-  -1*(y - a_3)*derivSg(z_3)
+    
+    delta_2 <- t(W_2)%*%t(delta_3) * derivSg(t(z_2))
+    
+    nabla_w_1 = delta_2 %*% a_1
+    nabla_b_1 = delta_2 
+    
+    nabla_w_2 = t(delta_3) %*% a_2
+    nabla_b_2 = t(delta_3)
+    
+    delta_W_1 <-  delta_W_1 + nabla_w_1
+    delta_W_2 <-  delta_W_2 + nabla_w_2
+    delta_b_1 <-  delta_b_1 + nabla_b_1
+    delta_b_2 <-  delta_b_2 + nabla_b_2
+    
+  }
+  
+  W_1 = W_1 + alpha * (delta_W_1 / m + lambda * W_1)
+  W_2 = W_2 + alpha * (delta_W_2 / m + lambda * W_2)
+  b1 = b1 + alpha * (delta_b_1 / m)
+  b2 = b2 + alpha * (delta_b_2 / m)
+  
+  n = n+1 
+  print(n)
+  print(abs(max(delta_W_1)))
+  print(abs(max(delta_W_2)))
+  print(abs(max(delta_b_1)))
+  print(abs(max(delta_b_2)))
+  maxComponent = max(abs(max(delta_W_1)),abs(max(delta_W_2)),abs(max(delta_b_1)),abs(max(delta_b_2)))
+  
+  #print(abs(max(delta)))
+  #print(norm(delta, "F"))
+}
+}
+
 
 
 
