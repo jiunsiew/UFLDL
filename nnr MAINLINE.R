0d3cb2dc6196f7ca98c219d1d930b45a7a4d9d6b
Plutonicx <stojanovich.stevan@live.com>
2015-09-10 22:00:28 +1000

    added in definitive solution. Looks like grad descent converges on local minimum

--- nnr MAINLINE.R
+++ nnr MAINLINE.R
@@ -35,7 +35,7 @@ X <- as.matrix(train[,c(5,6,8)])
 
 
 
-nneurons <- ncol(x)
+nneurons <- ncol(X)
 
 
 sg = function(x){
@@ -45,6 +45,30 @@ sg = function(x){
 derivSg = function(x){
   sg(x)*(1-sg(x))
 }
+
+# create fake y.
+
+
+w_1_real <- matrix(c(-0.8,0.5,0.1,-0.6,1.2,-0.54,1,-0.88,-0.32), nrow = 3, ncol = 3, byrow = TRUE)
+b1_real <- t(matrix(c(0.2,0,0), nrow = 1, ncol = 3, byrow = TRUE))
+
+w_2_real <- matrix(c(0.52,-1.65,0.11), nrow = 1, ncol = 3, byrow = TRUE)
+b2_real <- matrix(c(0.25))
+
+
+for (i in 1:nrow(X)){
+x = matrix(X[i,])
+a_1 = x
+
+z_2 = t(w_1_real %*% x + b1_real)
+a_2 = sg(z_2)
+
+
+z_3 = t(w_2_real %*% t(a_2) + b2_real)
+Y[i] = sg(z_3)
+}
+
+
 
 
 set.seed(823)
@@ -58,44 +82,53 @@ W_1 <- matrix(W1, nrow = 3, ncol = 3)
 W_2 <- t(as.matrix(rnorm(nneurons, mean = 0, sd = 1)))
 b2 <- as.matrix(rnorm(1, mean = 0, sd = 1))
 
-delta_W_1 = matrix(0, dim(W_1)[1], dim(W_1)[2] )
-delta_b_1 = matrix(0, dim(b1))
 
-delta_W_2 = matrix(0, dim(W_2)[1], dim(W_2)[2] )
-delta_b_2 = matrix(0, dim(b2))
+
+W_1 = w_1_real
+#W_2 = w_2_real
+b1 = b1_real
+#b2 = b2_real
 
 
 m = nrow(X)
-lambda = 1
+lambda = 0
 alpha = 0.005
 
 # counter to make sure we don't run an infinite loop.
 n = 0
 maxComponent = 1
-maxIterations = 100
+maxIterations = 2000
 #gradient descent. TODO: When delta gets ~ < 10 the algorithm becomes really slow to converge. Might need to dynamically increase alpha depending on the size of norm(delta, "F")
 
 
-gradDescent()
 
-gradDescent = function(){
-while (maxComponent > 0.0001 & n < maxIterations)
+
+#gradDescent = function(){
+while (maxComponent > 0.02 & n < maxIterations)
 {
+  
+  delta_W_1 = matrix(0, dim(W_1)[1], dim(W_1)[2] )
+  delta_b_1 = matrix(0, dim(b1))
+  
+  delta_W_2 = matrix(0, dim(W_2)[1], dim(W_2)[2] )
+  delta_b_2 = matrix(0, dim(b2))
+  
+  
   for (i in 1:nrow(X)){
     
     x = X[i,]
-    y = Y[i] / max(Y)
+    y = Y[i]
     #a_1 <- t(W_1 %*% t(x) + b1)
     
     a_1 = x
     
     
     
-    z_2 = t(W_1 %*% x + b1)
+    z_2 = W_1 %*% x + b1
     a_2 = sg(z_2)
     
     
-    z_3 = t(W_2 %*% t(a_2) + b2)
+    z_3 = t(W_2 %*% a_2 + b2)
     a_3 <- sg(z_3)
     
     
@@ -106,13 +139,13 @@ while (maxComponent > 0.0001 & n < maxIterations)
     
     delta_3 <-  -1*(y - a_3)*derivSg(z_3)
     
-    delta_2 <- t(W_2)%*%t(delta_3) * derivSg(t(z_2))
+    delta_2 <- t(W_2)%*%delta_3 * derivSg(z_2)
     
-    nabla_w_1 = delta_2 %*% a_1
+    nabla_w_1 = delta_2 %*% t(a_1)
     nabla_b_1 = delta_2 
     
-    nabla_w_2 = t(delta_3) %*% a_2
-    nabla_b_2 = t(delta_3)
+    nabla_w_2 = delta_3 %*% t(a_2)
+    nabla_b_2 = delta_3
     
     delta_W_1 <-  delta_W_1 + nabla_w_1
     delta_W_2 <-  delta_W_2 + nabla_w_2
@@ -121,10 +154,10 @@ while (maxComponent > 0.0001 & n < maxIterations)
     
   }
   
-  W_1 = W_1 + alpha * (delta_W_1 / m + lambda * W_1)
-  W_2 = W_2 + alpha * (delta_W_2 / m + lambda * W_2)
-  b1 = b1 + alpha * (delta_b_1 / m)
-  b2 = b2 + alpha * (delta_b_2 / m)
+  W_1 = W_1 - alpha * (delta_W_1 / m + lambda * W_1)
+  W_2 = W_2 - alpha * (delta_W_2 / m + lambda * W_2)
+  b1 = b1 - alpha * (delta_b_1 / m)
+  b2 = b2 - alpha * (delta_b_2 / m)
   
   n = n+1 
   print(n)
@@ -137,7 +170,7 @@ while (maxComponent > 0.0001 & n < maxIterations)
   #print(abs(max(delta)))
   #print(norm(delta, "F"))
 }
-}
+#}
 
 
 
