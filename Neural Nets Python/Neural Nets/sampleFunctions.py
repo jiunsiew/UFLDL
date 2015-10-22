cc7e428e2a5f192c53f5247923bf6b8e65d5a140
Plutonicx <stojanovich.stevan@live.com>
2015-10-22 20:36:26 +1100

    Neural networks code (python VS'15)

---
+++ Neural Nets Python/Neural Nets/sampleFunctions.py
@@ -0,0 +1,45 @@
+﻿
+#>>> import tester as test
+#>>> test.run_NN()
+#Epoch 0: 9046 / 10000
+#Epoch 1: 9232 / 10000
+#Epoch 2: 9255 / 10000
+#Epoch 3: 9305 / 10000
+#Epoch 4: 9340 / 10000
+#Epoch 5: 9353 / 10000
+#Epoch 6: 9380 / 10000
+#Epoch 7: 9391 / 10000
+#Epoch 8: 9385 / 10000
+#Epoch 9: 9438 / 10000
+#>>> import sampleFunctions as funcs
+#>>> funcs.showInput(test.Test,34)
+
+
+
+class sampleFunctions(object):
+    """description of class"""
+
+    
+import numpy as np
+from matplotlib.pyplot import matshow
+from matplotlib.pyplot import show
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import Neural_Nets as nn
+
+
+def predict(nets,matrices,i):
+    a = matrices[i][0]
+    for b, w in zip(nets.biases, nets.weights):
+        a = nn.sigmoid(np.dot(w, a).reshape(b.size,1) + b)
+    return np.argmax(a)
+
+def showInput(matrices,i):
+    x,y = matrices[i]
+    print y
+    matshow(np.asmatrix(x).reshape(28,28), fignum = 99, cmap = plt.get_cmap('gray') )
+    show()
+    return y
+
+
+
---
+++ Neural Nets Python/Neural Nets/tester.py
@@ -0,0 +1,53 @@
+﻿
+#import numpy as np
+#import pandas as pan
+
+
+#import mnist_loader   
+
+
+#mn = mnist_loader
+
+#Data = mn.load_data()
+
+#Train = Data[0]
+
+#Test = Data[2]
+
+#x = Train[0]
+#y = Train[1]
+
+#i = 5
+
+## For Plotting.
+##matshow(np.asmatrix(x[i]).reshape(28,28), fignum = 99, cmap = cm.gray)
+##show()
+## predictor value.
+
+#realTrain, realValidation, realTest = mn.load_data_wrapper()
+
+#import Neural_Nets as nn
+
+#nets = nn.Network([784,30,10])
+#nets.SGD(realTrain, 10, 10, 3.0, test_data = realTest)
+
+##prediction
+#tx, ty = realTest[i]
+#argmax(nets.feedforward(x))
+#y
+
+import Neural_Nets as nn
+nets = nn.Network([784,30,10])
+import mnist_loader
+Train, Val, Test = mnist_loader.load_data_wrapper()
+
+def run_NN():
+
+    nets.SGD(Train, 10, 10, 3.0, test_data = Test)
+
+
+#import mnist_loader
+#import sampleFunctions
+
+#sampleFunctions.showInput(Test, 3711)
+
